04cfe5d7353bc27622377d5ecceead2d18c98290
devgupta4526 <devguptamsi@gmail.com>
2024-07-09 11:23:57 +0530

    Completed Retrofit Setup Api Integration and Mapper

--- app/src/main/java/com/underground/picprism/utils/Constants.kt
+++ app/src/main/java/com/underground/picprism/utils/Constants.kt
@@ -2,9 +2,9 @@ package com.underground.picprism.utils
 
 object Constants {
 
-    const val API_KEY = "AX4L8mZmdHsRhkVkINPMczyjw41Vo2KUz2olZPtW31w"
+    const val API_KEY = ""
 
-    const val SECRET_KEY = "gZAhVVcLpDpJrj0TEicCsul5n3dq1R0Gnv7uon7TM7k"
+    const val SECRET_KEY = ""
 
     const val BASE_URL = "https://api.unsplash.com/"
 }
